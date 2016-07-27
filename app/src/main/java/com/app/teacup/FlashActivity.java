7f1f80b8969056c442ed4b5225e718b0a8692991
unknown <Administrator@SKY-20150711DQF.(none)>
2016-07-27 21:57:39 +0800

    add flash activity

---
+++ app/src/main/java/com/app/teacup/FlashActivity.java
@@ -0,0 +1,29 @@
+package com.app.teacup;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class FlashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_flash_activity);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(FlashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
