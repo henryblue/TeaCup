fb3e259858502a29ea03444076f00f676938d70a
unknown <Administrator@SKY-20150711DQF.(none)>
2016-08-10 23:20:59 +0800

    for music page

--- app/src/main/java/com/app/teacup/MusicDetailActivity.java
+++ app/src/main/java/com/app/teacup/MusicDetailActivity.java
@@ -1,13 +1,21 @@
 package com.app.teacup;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.bean.Music.MusicDetail;
 import com.app.bean.Music.MusicDetailInfo;
@@ -27,8 +35,32 @@ import java.util.List;
 public class MusicDetailActivity extends AppCompatActivity {
 
     private static final String TAG = "MusicDetailActivity";
+    private static final int LOAD_DATA_FINISH = 0;
+    private static final int LOAD_DATA_ERROR = 1;
+
     private MusicInfo mMusicInfo;
     private MusicDetailInfo mDetailInfo;
+    private TextView mMusicTitle;
+    private TextView mMusicType;
+    private TextView mMusicContent;
+    private LinearLayout mPlayList;
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case LOAD_DATA_FINISH:
+                    initData();
+                    break;
+                case LOAD_DATA_ERROR:
+                    Toast.makeText(MusicDetailActivity.this, "加载数据失败, 请检查网络",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,36 +95,80 @@ public class MusicDetailActivity extends AppCompatActivity {
         }
 
         ImageView ivImage = (ImageView) findViewById(R.id.iv_music_image);
-        Glide.with(this).load(mMusicInfo.getImgUrl())
-                .error(R.drawable.photo_loaderror)
-                .dontAnimate()
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .crossFade()
-                .into(ivImage);
+        if (ivImage != null) {
+            Glide.with(this).load(mMusicInfo.getImgUrl())
+                    .error(R.drawable.photo_loaderror)
+                    .dontAnimate()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .crossFade()
+                    .into(ivImage);
+        }
+
+        mMusicTitle = (TextView) findViewById(R.id.tv_music_title);
+        mMusicType = (TextView) findViewById(R.id.tv_music_type);
+        mMusicContent = (TextView) findViewById(R.id.tv_music_content);
+        mPlayList = (LinearLayout) findViewById(R.id.ll_play_list);
+
     }
 
     private void initData() {
+        mMusicTitle.setText(mMusicInfo.getTitle());
+        mMusicType.setText(mDetailInfo.getType());
+        mMusicContent.setText(mDetailInfo.getContent());
+
+        initLayout();
+    }
+
+    private void initLayout() {
+        List<MusicDetail> list = mDetailInfo.getMusicList();
+        for (int  i = 0; i < list.size(); i++) {
+            View view = View.inflate(MusicDetailActivity.this, R.layout.item_music_detail, null);
+            TextView index = (TextView) view.findViewById(R.id.tv_play_index);
+            ImageView img = (ImageView) view.findViewById(R.id.iv_play_img);
+            TextView name = (TextView) view.findViewById(R.id.tv_play_name);
+            TextView user = (TextView) view.findViewById(R.id.tv_play_user);
+
+            index.setText(String.format("%02d", i + 1));
+            Glide.with(this).load(list.get(i).getImgUrl())
+                    .error(R.drawable.photo_loaderror)
+                    .dontAnimate()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .crossFade()
+                    .into(img);
+            name.setText(list.get(i).getMusicName());
+            user.setText(list.get(i).getMusicPlayer());
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //TODO
+                }
+            });
+
+            mPlayList.addView(view);
+        }
     }
 
     private void startLoadData() {
-        HttpUtils.sendHttpRequest(mMusicInfo.getNextUrl(), new HttpUtils.HttpCallBackListener() {
+        HttpUtils.sendHttpRequest(mMusicInfo.getNextUrl(),
+                new HttpUtils.HttpCallBackListener() {
             @Override
             public void onFinish(String response) {
                 parseMusicData(response);
-                Handler handler = new Handler();
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        initData();
-                    }
-                });
+                sendParseMessage(LOAD_DATA_FINISH);
             }
 
             @Override
             public void onError(Exception e) {
                 e.printStackTrace();
+                sendParseMessage(LOAD_DATA_ERROR);
             }
         });
+    }
+
+    private void sendParseMessage(int what) {
+        Message msg = Message.obtain();
+        msg.what = what;
+        mHandler.sendMessage(msg);
     }
 
     private void parseMusicData(String response) {
@@ -122,6 +198,25 @@ public class MusicDetailActivity extends AppCompatActivity {
             music.setMusicPlayer(trackMeta.text());
             musicList.add(music);
         }
+
+        String id = mMusicInfo.getTitle().substring(4, 7);
+        for (int i = 1; i <= musicList.size(); i++) {
+            String num = String.format("%02d", i);
+            String musicUrl = "http://luoo-mp3.kssws.ks-cdn.com/low/luoo/radio" +
+                    id + "/" + num + ".mp3";
+            musicList.get(i - 1).setMusicUrl(musicUrl);
+        }
+
         mDetailInfo.setMusicList(musicList);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mHandler != null) {
+            mHandler.removeMessages(LOAD_DATA_FINISH);
+            mHandler.removeMessages(LOAD_DATA_ERROR);
+            mHandler = null;
+        }
     }
 }
