4747089f96fd7afed2c3cb996473d4974fee39cd
henry-blue <2298911475@qq.com>
2016-10-05 22:44:58 +0800

    rm update module

--- app/src/main/java/com/app/util/JsonUtils.java
+++ app/src/main/java/com/app/util/JsonUtils.java
@@ -1,12 +1,9 @@
 package com.app.util;
 
 
-import com.app.bean.UpdateInfo;
 import com.app.bean.book.Book;
 import com.google.gson.Gson;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class JsonUtils {
 
@@ -14,21 +11,6 @@ public class JsonUtils {
 
     public static Book parseJsonData(String jsonStr) {
         Book info = gson.fromJson(jsonStr, Book.class);
-        return info;
-    }
-
-    public static UpdateInfo parseUpdateJsonData(String jsonStr) throws JSONException {
-        UpdateInfo info = new UpdateInfo();
-        JSONObject object = new JSONObject(jsonStr);
-        String version = object.getString("version");
-        String name = object.getString("apkName");
-        String dec = object.getString("description");
-        String url = object.getString("apkUrl");
-
-        info.setVersion(version);
-        info.setApkName(name);
-        info.setDec(dec);
-        info.setUrl(url);
         return info;
     }
 }
